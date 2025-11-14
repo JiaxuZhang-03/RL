1bcf648821de1fedd3114250ad1dbf619a0b95ec
Jiaxu Zhang <jiaxu_zhang@berkeley.edu>
2025-11-13 20:17:12 -0800

    Add Files to the repo

---
+++ DQN/double_dqn.py
@@ -0,0 +1,46 @@
+import random
+import gymnasium as gym
+import numpy as np
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import rl_utils
+from tqdm import tqdm
+
+class Qnet(torch.nn.Module):
+    def __init__(self,state_dim,hidden_dim,action_dim):
+        super(Qnet,self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim,hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim,action_dim)
+    def forward(self,x):
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+    
+
+class DQN:
+    def __init__(self,state_dim,hidden_dim,action_dim,learning_rate,gamma,epsilon,target_update,device,dqn_type = 'VanillaDQN'):
+        self.action_dim = action_dim
+        self.q_net = Qnet(state_dim,hidden_dim,self.action_dim).to(device)
+        self.optimizer = torch.optim.Adam(self.q_net.parameters(),lr = learning_rate)
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.target_update = target_update
+        self.count = 0
+        self.dqn_type = dqn_type
+        self.device = device
+
+    def take_action(self,state):
+        if np.random.random() < self.epsilon:
+            action = np.random.randint(self.action_dim)
+        else:
+            state = torch.tensor([state],dtype = torch.float).to(self.device)
+            action = self.q_net(state).argmax().item()
+        return action
+    
+    def max_q_value(self,state):
+        state = torch.tensor([state],dtype = torch.float).to(self.device)
+        return self.q_net(state).max().item()
+
+    def update(self,transition_dict):
+        states = torch.tensor(transition_dict['states'],dtype = torch.float).to(self.device)
+        actions = torch.tensor(transition_dict['actions']).view(-1,1).to(self.device)
